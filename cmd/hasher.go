6a332abde5b2df371e59a262b85d0b5cb90dc355
jellybro99 <benluke1111@gmail.com>
2025-12-29 22:07:22 -0600

    moved hasher to separate file and created hash formatting method

---
+++ cmd/hasher.go
@@ -0,0 +1,75 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/jellybro99/sha256_cli/sha256"
+	"github.com/spf13/cobra"
+)
+
+func runHasher(cmd *cobra.Command, args []string) {
+	hashFunction, err := cmd.Flags().GetString("hash")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	messages, err := getInputs(args)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	if len(messages) == 0 {
+		fmt.Println("no input given")
+	}
+
+	switch hashFunction {
+	case "sha256":
+		if len(messages) == 1 {
+			fmt.Println(formatHash(sha256.Hash(messages[0])))
+			break
+		}
+		for _, message := range messages {
+			fmt.Printf("%s: %s\n", message, formatHash(sha256.Hash(message)))
+		}
+	default:
+		fmt.Println("Given hash is not supported")
+	}
+}
+
+func formatHash(hash [8]uint32) string {
+	var sb strings.Builder
+
+	for _, section := range hash {
+		fmt.Fprintf(&sb, "%X ", section)
+	}
+
+	return sb.String()
+}
+
+func getInputs(args []string) ([]string, error) {
+	if len(args) > 0 {
+		return args, nil
+	}
+
+	file, err := os.Stdin.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	if file.Size() == 0 {
+		return nil, errors.New("no input")
+	}
+
+	message, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		return nil, err
+	}
+
+	return []string{string(message)}, nil
+}
--- cmd/root.go
+++ cmd/root.go
@@ -2,12 +2,8 @@
 package cmd
 
 import (
-	"errors"
-	"fmt"
-	"io"
 	"os"
 
-	"github.com/jellybro99/sha256_cli/sha256"
 	"github.com/spf13/cobra"
 )
 
@@ -32,57 +28,4 @@ func Execute() {
 
 func init() {
 	rootCmd.Flags().String("hash", "sha256", "Use the given hash function. Currently supported: sha256")
-}
-
-func runHasher(cmd *cobra.Command, args []string) {
-	hashFunction, err := cmd.Flags().GetString("hash")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	messages, err := getInputs(args)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	if len(messages) == 0 {
-		fmt.Println("no input given")
-	}
-
-	switch hashFunction {
-	case "sha256":
-		if len(messages) == 1 {
-			fmt.Printf("%X\n", sha256.Hash(messages[0]))
-			break
-		}
-		for _, message := range messages {
-			fmt.Printf("%s: %X\n", message, sha256.Hash(message))
-		}
-	default:
-		fmt.Println("Given hash is not supported")
-	}
-}
-
-func getInputs(args []string) ([]string, error) {
-	if len(args) > 0 {
-		return args, nil
-	}
-
-	file, err := os.Stdin.Stat()
-	if err != nil {
-		return nil, err
-	}
-
-	if file.Size() == 0 {
-		return nil, errors.New("no input")
-	}
-
-	message, err := io.ReadAll(os.Stdin)
-	if err != nil {
-		return nil, err
-	}
-
-	return []string{string(message)}, nil
 }
