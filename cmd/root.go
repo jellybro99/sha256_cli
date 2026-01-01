157cf28ef7f3a5983763e8d1f5b267dce4ec2cda
jellybro99 <benluke1111@gmail.com>
2026-01-01 09:27:54 -0600

    changed error handling

--- cmd/root.go
+++ cmd/root.go
@@ -2,8 +2,6 @@
 package cmd
 
 import (
-	"os"
-
 	"github.com/spf13/cobra"
 )
 
@@ -11,19 +9,15 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "sha",
 	Short: "A go implementation of the sha256 hashing algorithm",
-	Long: `sha is a CLI tool for the sha256 hashing algorithm.
-	You can provide text as arguments or pipe it in via stdin.`,
-	Args: cobra.ArbitraryArgs,
-	Run:  runHasher,
+	Long:  "sha is a CLI tool for the sha256 hashing algorithm. You can provide text as arguments or pipe it in via stdin.",
+	Args:  cobra.ArbitraryArgs,
+	Run:   runHasher,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
-		os.Exit(1)
-	}
+func Execute() error {
+	return rootCmd.Execute()
 }
 
 func init() {
